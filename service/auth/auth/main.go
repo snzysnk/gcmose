ff466abe2293b82af1e0fba22c263271071646c8
xieruixiang <1046421940@qq.com>
2022-01-17 17:21:04 +0800

    修改auth服务的创建方式

--- service/auth/auth/main.go
+++ service/auth/auth/main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/rsa"
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
-	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -13,13 +12,11 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	"google.golang.org/protobuf/encoding/protojson"
 	"io/ioutil"
-	"net"
-	"net/http"
 	wechatpb "project/service/auth/api"
 	"project/service/auth/dao"
 	"project/service/auth/wechat"
+	grpc_help "project/service/shared/grpc"
 	"project/service/shared/token"
 	"time"
 )
@@ -85,64 +82,37 @@ func main() {
 }
 
 func createRpcService() {
-	//监听端口
-	listen, err := net.Listen("tcp", ":9001")
-	if err != nil {
-		panic(err)
-	}
 
-	server := grpc.NewServer()
-	connect, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
-	if err != nil {
-		panic(err)
-	}
+	manger := grpc_help.ServiceManger{
+		NewWork: "tcp",
+		Port:    "9001",
+		CallBack: func(server grpc.ServiceRegistrar) {
+			connect, err := mongo.Connect(context.Background(), options.Client().ApplyURI("mongodb://localhost:27017"))
+			if err != nil {
+				panic(err)
+			}
 
-	wechatpb.RegisterLoginServiceServer(server, &Service{Ws: wechat.Service{
-		AppId:     "wxace898a3c3893f74",
-		AppSecret: "6961b9bd55ca8b4ff3aed79af448d7c3",
-	}, mg: dao.NewMongo(connect.Database("cool"), context.Background(), primitive.NewObjectID), jt: token.JWTToken{
-		NowFunc: func() time.Time {
-			return time.Now()
+			wechatpb.RegisterLoginServiceServer(server, &Service{Ws: wechat.Service{
+				AppId:     "wxace898a3c3893f74",
+				AppSecret: "6961b9bd55ca8b4ff3aed79af448d7c3",
+			}, mg: dao.NewMongo(connect.Database("cool"), context.Background(), primitive.NewObjectID), jt: token.JWTToken{
+				NowFunc: func() time.Time {
+					return time.Now()
+				},
+				PrivateKey:   ReadPem(privateKeyPath),
+				GetPublicKey: nil,
+			}})
 		},
-		PrivateKey:   ReadPem(privateKeyPath),
-		GetPublicKey: nil,
-	}})
-	server.Serve(listen)
+	}
+
+	manger.Start()
 }
 
 func createGateway() {
-
-	//可以取消的上下文
-	//为啥用context.WithCancel,因为这个服务一般在协程中启动,可以在某个协程异常时取消其关联的上下文
-	background := context.Background()
-	c, cancelFunc := context.WithCancel(background)
-	defer cancelFunc()
-
-	//通过runtime.NewServeMux 创建一个http服务器
-	//runtime.NewServeMux 只有一个opts 参数 即服务器参数
-	//runtime.WithMarshalerOption 返回一个转换关系的服务器参数
-	//runtime.MIMEWildcard 即 "*" 对所有数据应用转换关系
-	//runtime.JSONPb 对数据转换成json 时的 转换关系
-	//MarshalOptions 具体关系参数
-	//protojson.MarshalOptions 当proto 转换成 json 时 的规则
-
-	mux := runtime.NewServeMux(runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
-		MarshalOptions: protojson.MarshalOptions{
-			UseEnumNumbers: true, //转换枚举变量 输出枚举变量值,false 则输出枚举变量名
-			UseProtoNames:  true, //响应body 中 使用 proto 中声明的变量名,不然其用_组成的变量名变成大驼峰形式 如 my_logo => myLogo
-			AllowPartial:   true, //允许请求报文中缺少字段
-		},
-	}))
-
-	//将grpc 服务器 与 grpc-gateway 关联
-	//RegisterLoginServiceHandlerFromEndpoint  LoginService 是在proto 中 声明的服务名称 如果你声明的叫Xxx 那么这个方法现在就叫RegisterXxxHandlerFromEndpoint
-	//第一个参数是传递的上下文
-	//第二个参数是 http服务器
-	//第三个参数是 grpc 服务端的端口
-	//第四个参数是 拨号参数 这里使用不安全的连接方式
-	//http 服务器与grpc 9001端口进行关联。通过拨号的方式进行通信
-	wechatpb.RegisterLoginServiceHandlerFromEndpoint(c, mux, ":9001", []grpc.DialOption{grpc.WithInsecure()})
-
-	//通过http.ListenAndServe 在本地 9002 端口 启动http服务器
-	http.ListenAndServe("localhost:9002", mux)
+	manager := grpc_help.GateWayManager{
+		Port: "9002",
+		Mux:  grpc_help.CreateBaseMux(),
+	}
+	manager.RegisterPoint(wechatpb.RegisterLoginServiceHandlerFromEndpoint, "9001")
+	manager.Start()
 }
